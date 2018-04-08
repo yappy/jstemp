9140ded897361fe8ed50f84bba8d834f724ffcd6
yappy <superunyo@yahoo.co.jp>
2018-04-08 14:19:34 +0900

    Render fps

--- src/js/game/game.js
+++ src/js/game/game.js
@@ -1,32 +1,37 @@
 export class GameApplication {
   constructor(canvas, fps) {
-    this.canvas_ = canvas;
-    this.context_ = canvas.getContext("2d");
+    this.canvas = canvas;
+    this.context = canvas.getContext("2d");
     this.fps_ = fps;
     this.frameMs_ = 1000 / fps;
 
     this.frameCount_ = 0;
     this.baseTime_ = new Date().getTime();
-    this.printFps_ = 0.0;
+    this.printFps_ = "0.0";
   }
 
   update() {
     this.frameCount_++;
     if (this.frameCount_ >= this.fps_) {
       const now = new Date().getTime();
-      this.printFps_ = this.frameCount_ / (now - this.baseTime_) * 1000.0;
+      const fps = this.frameCount_ / (now - this.baseTime_) * 1000.0;
+      this.printFps_ = fps.toFixed(1);
       this.baseTime_ = now;
       this.frameCount_ = 0;
     }
   }
 
   render() {
-    const ctx = this.context_;
+    const ctx = this.context;
+    const w = this.canvas.width;
+    const h = this.canvas.height;
+
     ctx.fillStyle = "#0000FF";
-    ctx.fillRect(0, 0, this.canvas_.width, this.canvas_.height);
-    if (this.frameCount_ == 0) {
-      console.log(this.printFps_);
-    }
+    ctx.fillRect(0, 0, w, h);
+
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "10px serif";
+    ctx.fillText(this.printFps_, w - 30, h - 10);
   }
 
   frame_(startMs) {
